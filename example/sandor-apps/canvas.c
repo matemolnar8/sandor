4ee6cb407ae2a8f27d321e4855ba2019c83905f5
matemolnar8 <doglotteger@gmail.com>
2025-07-16 21:08:56 +0200

    Naming for cube canvas

--- example/sandor-apps/canvas.c
+++ example/sandor-apps/canvas.c
@@ -13,35 +13,34 @@ float cosf(float x);
 
 #define PI 3.14159265359
 
-#define BACKGROUND_COLOR 0xFF181818
-#define GRID_COUNT 10
-#define GRID_PAD 0.5/GRID_COUNT
-#define GRID_SIZE ((GRID_COUNT - 1)*GRID_PAD)
-#define CIRCLE_RADIUS 4
-#define Z_START 0.4
+#define CUBE_BACKGROUND_COLOR 0xFF181818
+#define CUBE_GRID_COUNT 10
+#define CUBE_GRID_PAD 0.5/CUBE_GRID_COUNT
+#define CUBE_GRID_SIZE ((CUBE_GRID_COUNT - 1)*CUBE_GRID_PAD)
+#define CUBE_CIRCLE_RADIUS 4
+#define CUBE_Z_START 0.4
 
-uint32_t pixels[WIDTH*HEIGHT];
-Olivec_Canvas demo_canvas = OLIVEC_CANVAS_NULL;
+uint32_t cube_pixels[WIDTH*HEIGHT];
+Olivec_Canvas cube_canvas = OLIVEC_CANVAS_NULL;
+static float cube_angle = 0;
 
-static float angle = 0;
-
-// Copied from Tsoding olive.c example
-Olivec_Canvas vc_render(float dt)
+// Copied from Tsoding Olive.c example
+Olivec_Canvas render_cube(float dt)
 {
-    angle += 0.25*PI*dt;
+    cube_angle += 0.25*PI*dt;
 
-    Olivec_Canvas oc = olivec_canvas(pixels, WIDTH, HEIGHT, WIDTH);
+    Olivec_Canvas oc = olivec_canvas(cube_pixels, WIDTH, HEIGHT, WIDTH);
 
-    olivec_fill(oc, BACKGROUND_COLOR);
-    for (int ix = 0; ix < GRID_COUNT; ++ix) {
-        for (int iy = 0; iy < GRID_COUNT; ++iy) {
-            for (int iz = 0; iz < GRID_COUNT; ++iz) {
-                float x = ix*GRID_PAD - GRID_SIZE/2;
-                float y = iy*GRID_PAD - GRID_SIZE/2;
-                float z = Z_START + iz*GRID_PAD;
+    olivec_fill(oc, CUBE_BACKGROUND_COLOR);
+    for (int ix = 0; ix < CUBE_GRID_COUNT; ++ix) {
+        for (int iy = 0; iy < CUBE_GRID_COUNT; ++iy) {
+            for (int iz = 0; iz < CUBE_GRID_COUNT; ++iz) {
+                float x = ix*CUBE_GRID_PAD - CUBE_GRID_SIZE/2;
+                float y = iy*CUBE_GRID_PAD - CUBE_GRID_SIZE/2;
+                float z = CUBE_Z_START + iz*CUBE_GRID_PAD;
 
                 float cx = 0.0;
-                float cz = Z_START + GRID_SIZE/2;
+                float cz = CUBE_Z_START + CUBE_GRID_SIZE/2;
 
                 float dx = x - cx;
                 float dz = z - cz;
@@ -49,8 +48,8 @@ Olivec_Canvas vc_render(float dt)
                 float a = atan2f(dz, dx);
                 float m = sqrtf(dx*dx + dz*dz);
 
-                dx = cosf(a + angle)*m;
-                dz = sinf(a + angle)*m;
+                dx = cosf(a + cube_angle)*m;
+                dz = sinf(a + cube_angle)*m;
 
                 x = dx + cx;
                 z = dz + cz;
@@ -58,11 +57,11 @@ Olivec_Canvas vc_render(float dt)
                 x /= z;
                 y /= z;
 
-                uint32_t r = ix*255/GRID_COUNT;
-                uint32_t g = iy*255/GRID_COUNT;
-                uint32_t b = iz*255/GRID_COUNT;
+                uint32_t r = ix*255/CUBE_GRID_COUNT;
+                uint32_t g = iy*255/CUBE_GRID_COUNT;
+                uint32_t b = iz*255/CUBE_GRID_COUNT;
                 uint32_t color = 0xFF000000 | (r<<(0*8)) | (g<<(1*8)) | (b<<(2*8));
-                olivec_circle(oc, (x + 1)/2*WIDTH, (y + 1)/2*HEIGHT, CIRCLE_RADIUS, color);
+                olivec_circle(oc, (x + 1)/2*WIDTH, (y + 1)/2*HEIGHT, CUBE_CIRCLE_RADIUS, color);
             }
         }
     }
@@ -70,19 +69,16 @@ Olivec_Canvas vc_render(float dt)
     return oc;
 }
 
-char* canvas_id = "demo-canvas";
-
-void draw_canvas(float dt)
+char* canvas_id = "cube-canvas";
+void draw_cube_canvas(float dt)
 {
-    demo_canvas = vc_render(dt);
-
-    platform_draw_canvas(canvas_id, &demo_canvas);
+    cube_canvas = render_cube(dt);
+    platform_draw_canvas(canvas_id, &cube_canvas);
 }
 
 void init_component() {
     printf("Initializing Canvas Component\n");
-
-    platform_on_animation_frame(draw_canvas);
+    platform_on_animation_frame(draw_cube_canvas);
 }
 
 Element* render_component()
@@ -94,7 +90,7 @@ Element* render_component()
     return class(
         element("div", children(
             class(text_element("h1", "Canvas App"), "text-3xl font-bold mb-6"),
-            class(text_element("p", "A simple canvas element for drawing"), "text-lg text-gray-600 mb-4"),
+            class(text_element("p", "Interactive 3D cube animation"), "text-lg text-gray-600 mb-4"),
             text_element("p", arena_sprintf(&r_arena, "Render count: %d", render_count)),
             class(
                 canvas_element,
